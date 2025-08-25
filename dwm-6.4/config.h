a663c3ddb88f4fd3acfe6d42337690216f50c080
akai <akaihanatsundeee@proton.me>
2025-08-25 20:04:55 +0200

    organized and aligned all code, added more per-workspace rules to auto-sort programs on spawn

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -1,6 +1,6 @@
 /* akai's custom config */
 
-/* appearance */
+/* APPEARANCE */
 // windows
 static const unsigned int borderpx = 4;  // border pixel of windows
 static const unsigned int snap     = 32; // snap pixel
@@ -9,12 +9,12 @@ static const int topbar            = 0;  // 0 means bottom bar
 
 // font
 static const char *fonts[] = {
-	"FiraCode Nerd Font Propo Ret:size=10",
-        "Noto Sans CJK JP Medium:size=10"
+    "FiraCode Nerd Font Propo Ret:size=10",
+    "Noto Sans CJK JP Medium:size=10"
 };
 
 static const char dmenufont[] = {
-	"FiraCode Nerd Font Mono Ret:style=Regular:size=10"
+    "FiraCode Nerd Font Mono Ret:style=Regular:size=10"
 };
 
 // colorscheme
@@ -36,146 +36,161 @@ static const char *tags[] = {"贖", "罪", "へ", "の", "途", "上"};
 
 // workspace redirects
 static const Rule rules[] = {
-    /* class          instance title tags mask isfloating  monitor */
-    {"firefox",       NULL,    NULL, 1  <<  0, 0,          -1},
-    {"zen",           NULL,    NULL, 1  <<  0, 0,          -1},
-    {"zen-browser",   NULL,    NULL, 1  <<  0, 0,          -1},
-    {"discord",       NULL,    NULL, 1  <<  1, 0,          -1},
-    {"vesktop",       NULL,    NULL, 1  <<  1, 0,          -1},
-    {"equibop",       NULL,    NULL, 1  <<  1, 0,          -1},
-    {"java",          NULL,    NULL, 1  <<  3, 0,          -1},
-    {"minecraft",     NULL,    NULL, 1  <<  3, 0,          -1},
-    {"Emacs",         NULL,    NULL, 1  <<  4, 0,          -1},
-    {"prismlauncher", NULL,    NULL, 1  <<  5, 0,          -1},
-    {"corectrl",      NULL,    NULL, 1  <<  5, 0,          -1},
-    {"qbittorrent",   NULL,    NULL, 1  <<  5, 0,          -1},
+    /* class           instance title tags   mask isfloating monitor */
+    // 1
+    { "firefox",       NULL,    NULL, 1  <<  0,   0,         -1 },
+    { "firefox-bin",   NULL,    NULL, 1  <<  0,   0,         -1 },
+    { "zen",           NULL,    NULL, 1  <<  0,   0,         -1 },
+    { "zen-browser",   NULL,    NULL, 1  <<  0,   0,         -1 },
+    { "vivaldi",       NULL,    NULL, 1  <<  0,   0,         -1 },
+    { "vivaldi-bin",   NULL,    NULL, 1  <<  0,   0,         -1 },
+    // 2
+    { "discord",       NULL,    NULL, 1  <<  1,   0,         -1 },
+    { "vesktop",       NULL,    NULL, 1  <<  1,   0,         -1 },
+    { "equibop",       NULL,    NULL, 1  <<  1,   0,         -1 },
+    { "electron",      NULL,    NULL, 1  <<  1,   0,         -1 },
+    // 3
+    { "youtube-music", NULL,    NULL, 1  <<  1,   0,         -1 },
+    // 4
+    { "java",          NULL,    NULL, 1  <<  3,   0,         -1 },
+    { "minecraft",     NULL,    NULL, 1  <<  3,   0,         -1 },
+    // 5
+    { "emacs",         NULL,    NULL, 1  <<  4,   0,         -1 },
+    { "Emacs",         NULL,    NULL, 1  <<  4,   0,         -1 },
+    // 6
+    { "prismlauncher", NULL,    NULL, 1  <<  5,   0,         -1 },
+    { "corectrl",      NULL,    NULL, 1  <<  5,   0,         -1 },
+    { "qbittorrent",   NULL,    NULL, 1  <<  5,   0,         -1 },
+    { "deluge",        NULL,    NULL, 1  <<  5,   0,         -1 },
+    { "deluge-gtk",    NULL,    NULL, 1  <<  5,   0,         -1 },
 };
 
 /* layout(s) */
-static const float mfact        = 0.55; // factor of master area size [0.05..0.95]
-static const int nmaster        = 1;    // number of clients in master area
-static const int resizehints    = 1;    // 1 means respect size hints in tiled resizals
-static const int lockfullscreen = 0;    // 1 will force focus on the fullscreen window
+static const float mfact        = 0.5; // factor of master area size [0.05..0.95]
+static const int nmaster        = 1;   // number of clients in master area
+static const int resizehints    = 1;   // 1 means respect size hints in tiled resizals
+static const int lockfullscreen = 0;   // 1 will force focus on the fullscreen window
 
 static const Layout layouts[] = {
+    // first entry is default
+    // no layout function means floating behavior
+
     /* symbol     arrange function */
-    {"[]=",       tile},   // first entry is default
-    {"><>",       NULL},   // no layout function means floating behavior
-    {"[M]",       monocle},
+    { "[]=",       tile },
+    { "><>",       NULL },
+    { "[M]",       monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                                      \
-    {MODKEY, KEY, view, {.ui = 1 << TAG}},                                     \
-        {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},             \
-        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                      \
-        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+#define MODKEY Mod4Mask // Mod3Mask == Alt; Mod4Mask == Super 
+
+// basic window manipulation
+#define TAGKEYS(KEY, TAG) \
+        { MODKEY,                           KEY, view,       {.ui = 1 << TAG} }, \
+        { MODKEY | ControlMask,             KEY, toggleview, {.ui = 1 << TAG} }, \
+        { MODKEY | ShiftMask,               KEY, tag,        {.ui = 1 << TAG} }, \
+        { MODKEY | ControlMask | ShiftMask, KEY, toggletag,  {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                                             \
-    {                                                                          \
-        .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                   \
-    }
+#define SHCMD(cmd) { .v = (const char *[]) {"/bin/dash", "-c", cmd, NULL} }
 
 /* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0"; // component of dmenucmd, manipulated in spawn()
+
+// dmenu
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL
+};
 
 // custom placeholders
-static const char *browser-kill[]   = {"sh", "-c", "~/scripts/browser-kill.sh", NULL};
-
-static const char *termcmd[]      = {"sh", "-c", "ghostty", NULL};
-static const char *layoutswitch[] = {"sh", "-c", "~/scripts/layoutswitch.sh", NULL};
+static const char *termcmd[] = {"dash", "-c", "ghostty", NULL};
 
 /* bindkeys */
 static const Key keys[] = {
     // workspaces (6)
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_q, 3)
-        TAGKEYS(XK_w, 4) TAGKEYS(XK_e, 5)
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_q, 3) TAGKEYS(XK_w, 4) TAGKEYS(XK_e, 5)
 
     // important dwm stuff
-    { MODKEY, XK_Tab, spawn, { .v = dmenucmd } },
-    { MODKEY | ShiftMask, XK_Return, quit, {0} },
+    { MODKEY,             XK_Tab,    spawn,     {.v = dmenucmd} },
+    { MODKEY | ShiftMask, XK_Return, quit,      {0}             },
+    { MODKEY,             XK_b,      togglebar, {0}             },
 
     // apps
-    { MODKEY, 		  XK_a, spawn, SHCMD("~/scripts/browser.sh") },
-    { MODKEY | ShiftMask, XK_a, spawn, SHCMD("~/scripts/browser-kill.sh") },
+    { MODKEY,             XK_a, spawn, SHCMD("~/scripts/browser.sh") },
+    { MODKEY | ShiftMask, XK_a, spawn, SHCMD("killall zen-bin")      },
 
-    { MODKEY, 		  XK_s, spawn, SHCMD("vesktop") },
-    { MODKEY | ShiftMask, XK_s, spawn, SHCMD("killall vesktop") },
+    { MODKEY,             XK_s, spawn, SHCMD("vesktop")          },
+    { MODKEY | ShiftMask, XK_s, spawn, SHCMD("killall electron") },
 
-    { MODKEY, 		  XK_d, spawn, SHCMD("kitty") },
-    { MODKEY | ShiftMask, XK_d, spawn, SHCMD("killall kitty") },
+    { MODKEY,             XK_d, spawn, SHCMD("ghostty")         },
+    { MODKEY | ShiftMask, XK_d, spawn, SHCMD("killall ghostty") },
 
-    { MODKEY, 		  XK_f, togglefullscr, {0}},
-    { MODKEY | ShiftMask, XK_f, spawn, killclient, {0}},
+    { MODKEY,             XK_f, togglefullscr, {0} },
+    { MODKEY | ShiftMask, XK_f, killclient,    {0} },
 
-    { MODKEY, 		  XK_z, spawn, SHCMD("prismlauncher") },
+    { MODKEY,             XK_z, spawn, SHCMD("prismlauncher")         },
     { MODKEY | ShiftMask, XK_z, spawn, SHCMD("killall prismlauncher") },
 
-    { MODKEY, 		  XK_x, spawn, SHCMD("ddnet") },
-    { MODKEY | ShiftMask, XK_x, spawn, SHCMD("killall ddnet") },
+    { MODKEY,             XK_x, spawn, SHCMD("~/Games/t-client/DDNet") },
+    { MODKEY | ShiftMask, XK_x, spawn, SHCMD("killall DDNet")          },
 
-    { MODKEY, 		  XK_c, spawn, SHCMD("youtube-music") },
+    { MODKEY,             XK_c, spawn, SHCMD("youtube-music")         },
     { MODKEY | ShiftMask, XK_c, spawn, SHCMD("killall youtube-music") },
 
-    { MODKEY, 		  XK_v, spawn, SHCMD("deluge") },
+    { MODKEY,             XK_v, spawn, SHCMD("deluge")         },
     { MODKEY | ShiftMask, XK_v, spawn, SHCMD("killall deluge") },
 
-    {MODKEY, XK_b, togglebar, {0}},
+
     // awesomebar patch
-    {MODKEY, XK_j, focusstackvis, {.i = +1}},
-    {MODKEY, XK_k, focusstackvis, {.i = -1}},
-    {MODKEY | ShiftMask, XK_j, focusstackhid, {.i = +1}},
-    {MODKEY | ShiftMask, XK_k, focusstackhid, {.i = -1}},
+    { MODKEY,             XK_j, focusstackvis, {.i = +1} },
+    { MODKEY,             XK_k, focusstackvis, {.i = -1} },
+    { MODKEY | ShiftMask, XK_j, focusstackhid, {.i = +1} },
+    { MODKEY | ShiftMask, XK_k, focusstackhid, {.i = -1} },
 
     /* remove inc/dec master: i don't use it, takes up keybinding slots */
-    {MODKEY, XK_o, incnmaster, {.i = +1}},
-    {MODKEY, XK_p, incnmaster, {.i = -1}},
+    { MODKEY, XK_o, incnmaster, {.i = +1} },
+    { MODKEY, XK_p, incnmaster, {.i = -1} },
 
     // window control
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_grave, zoom, {0}},
-    {MODKEY | ShiftMask, XK_Tab, view, {0}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+    { MODKEY,             XK_h,      setmfact,       {.f = -0.05} },
+    { MODKEY,             XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY,             XK_grave,  zoom,           {0}          },
+    { MODKEY | ShiftMask, XK_Tab,    view,           {0}          },
+    { MODKEY,             XK_space,  setlayout,      {0}          },
+    { MODKEY | ShiftMask, XK_space,  togglefloating, {0}          },
+    { MODKEY,             XK_0,      view,           {.ui = ~0}   },
+    { MODKEY | ShiftMask, XK_0,      tag,            {.ui = ~0}   },
+    { MODKEY,             XK_comma,  focusmon,       {.i = -1}    },
+    { MODKEY,             XK_period, focusmon,       {.i = +1}    },
+    { MODKEY | ShiftMask, XK_comma,  tagmon,         {.i = -1}    },
+    { MODKEY | ShiftMask, XK_period, tagmon,         {.i = +1}    },
 
     // awesomebar patch
-    {MODKEY, XK_r, hide, {0}},
-    {MODKEY, XK_t, showall, {0}},
-    {MODKEY | ShiftMask, XK_t, show, {0}},
+    { MODKEY,             XK_r, hide,    {0} },
+    { MODKEY,             XK_t, showall, {0} },
+    { MODKEY | ShiftMask, XK_t, show,    {0} },
 
-	
+    
     // layout switching
-    { MODKEY, XK_4, spawn, {.v = layoutswitch} }
-
+    { MODKEY, XK_4, spawn, SHCMD("~/scripts/layoutswitch.sh") }
 };
 
 /* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
- * ClkClientWin, or ClkRootWin */
+
+// click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin
+
 static const Button buttons[] = {
     /* click eventmask button function argument */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
-    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
-    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+    { ClkLtSymbol,   0,      Button1, setlayout,      {0}                },
+    { ClkLtSymbol,   0,      Button3, setlayout,      {.v = &layouts[2]} },
+    { ClkWinTitle,   0,      Button2, zoom,           {0}                },
+    { ClkStatusText, 0,      Button2, spawn,          {.v = termcmd}     },
+    { ClkClientWin,  MODKEY, Button1, movemouse,      {0}                },
+    { ClkClientWin,  MODKEY, Button2, togglefloating, {0}                },
+    { ClkClientWin,  MODKEY, Button3, resizemouse,    {0}                },
+    { ClkTagBar,     0,      Button1, view,           {0}                },
+    { ClkTagBar,     0,      Button3, toggleview,     {0}                },
+    { ClkTagBar,     MODKEY, Button1, tag,            {0}                },
+    { ClkTagBar,     MODKEY, Button3, toggletag,      {0}                },
 };
