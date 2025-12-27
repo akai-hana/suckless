d191181ba306798c5fb1a475b0ac456dc5ff881c
akai-hana <akaihanatsundeee@proton.me>
2025-12-27 05:21:55 +0100

    automated sync

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -36,12 +36,11 @@ static const char *colors[][3] = {
 // transparency (alpha patch)
 #define OPAQUE      0xffU // U -> unsigned
 #define TRANSLUCENT 0x00U
-static const unsigned int baralpha = TRANSLUCENT; // bar background alpha
-static const unsigned int borderalpha = OPAQUE;      // border alpha
+static const unsigned int alpha = 0xf0U; // bar alpha
 
 static const unsigned int alphas[][3] = {
 	// fg          bg        border
-	{ borderalpha,    borderalpha, borderalpha }
+	{ baralpha,    baralpha, baralpha}
 };
 
 /* WORKSPACES */
